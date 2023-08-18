0b4248936c98448470fb033cf04ae0e61648af9a
unknown <charleseduardofaria@gmail.com>
2023-08-18 20:36:35 -0300

    Subindo primeiro projeto com js

--- html-css-js/03-js-basics/script.js
+++ html-css-js/03-js-basics/script.js
@@ -4,12 +4,13 @@ function loadDiv() {
 
   div.innerHTML = `<h1>Olá, mundo!</h1>`;
 
-  btn.innerHTML = "Descarregar";
+  btn.innerHTML = "Alterar";
 }
 
 function unloadDiv() {
   let div = document.getElementById("my-div");
-  div.innerHTML = "";
+
+  div.innerHTML = "<h1>Cervejas!</h1>";
 
   let btn = document.getElementById("load-btn");
   btn.innerHTML = "Carregar";
@@ -18,11 +19,47 @@ function unloadDiv() {
 function changeDivState() {
   let btn = document.getElementById("load-btn");
 
+  unfade(document.getElementById("my-div"));
+
   if (btn.innerHTML === "Carregar") {
     loadDiv();
   } else {
     unloadDiv();
   }
+}
+
+function fade(element) {
+  var op = 1;
+
+  var timer = setInterval(function () {
+    if (op <= 0.1) {
+      clearInterval(timer);
+
+      element.style.display = "none";
+    }
+
+    element.style.opacity = op;
+    element.style.filter = "alpha(opacity=" + op * 100 + ")";
+
+    op -= op * 0.1;
+  }, 50);
+}
+
+function unfade(element) {
+  var op = 0.1;
+
+  element.style.display = "block";
+
+  var timer = setInterval(function () {
+    if (op >= 1) {
+      clearInterval(timer);
+    }
+
+    element.style.opacity = op;
+    element.style.filter = "alpha(opacity=" + op * 100 + ")";
+
+    op += op * 0.1;
+  }, 20);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
