59eaba72ceb28e3a2072322accef089c8efcefe5
unknown <charleseduardofaria@gmail.com>
2023-10-23 22:42:31 -0300

    Finalizando, por enquanto, projeto next0

--- react-next-js/01-next0/.next/static/chunks/webpack.js
+++ react-next-js/01-next0/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0b2194e46002623"; }
+/******/ 		__webpack_require__.h = function() { return "9733cf036d7d463f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- react-next-js/01-next0/pages/index.js
+++ react-next-js/01-next0/pages/index.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { Header } from "./fragments/Header";
+import { Footer } from "./fragments/Footer";
 
 export default function Home() {
   return (
     <div>
+      <Header />
+
       <h2>Viva Santana!</h2>
       <h3>Olá, mundo!</h3>
       <Link href="/about">Sobre</Link>
       <br></br>
       <Link href="/form">Form</Link>
+
+      <Footer />
     </div>
   );
 }
