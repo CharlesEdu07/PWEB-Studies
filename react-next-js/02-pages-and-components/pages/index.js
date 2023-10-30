a2f21c8a5ab98f028c609742bd86148b4654e2ff
unknown <charleseduardofaria@gmail.com>
2023-10-30 09:47:15 -0300

    Subindo segundo projeto em NextJs

---
+++ react-next-js/02-pages-and-components/pages/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Header } from "./fragments/Header";
+import { Footer } from "./fragments/Footer";
+import { CustomPage } from "./fragments/custom-page";
+
+export default function Home() {
+  console.log("Vamos ver se você é brabo mesmo");
+
+  return (
+    <div>
+      <Header />
+      <h2>Viva Santana!</h2>
+      <h3>Olá, mundo!</h3>
+      <Link href="/about">Sobre</Link>
+      <br></br>
+      <Link href="/form">Form</Link>
+      <div>
+        <h1>Meu Aplicativo</h1>
+        <CustomPage text="Este é um texto passado por uma propriedade." />
+      </div>
+      <Footer />
+    </div>
+  );
+}
