ffbe5c04096f4c696789d5abd6c4cd9e224d5944
unknown <charleseduardofaria@gmail.com>
2023-10-30 12:08:23 -0300

    Organizando codigo

--- react-next-js/02-pages-and-components/pages/movies.js
+++ react-next-js/02-pages-and-components/pages/movies.js
@@ -6,6 +6,14 @@ export default function Movies({ data }) {
   );
 }
 
+function mapping(element) {
+  return (
+    <div>
+      {element.Title} --- {element.Year}
+    </div>
+  );
+}
+
 export async function getServerSideProps(context) {
   const res = await fetch(`http://www.omdbapi.com/?apikey=67b600f&s=bagdad`);
 
@@ -16,12 +24,4 @@ export async function getServerSideProps(context) {
       data,
     },
   };
-}
-
-function mapping(element) {
-  return (
-    <div>
-      {element.Title} --- {element.Year}
-    </div>
-  );
 }
