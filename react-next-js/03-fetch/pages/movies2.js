f0d3761bf0f9ca6c8ec42d094aa0536a7eea152e
unknown <charleseduardofaria@gmail.com>
2023-11-18 21:12:50 -0300

    Implementando busca na view carregada no cliente

--- react-next-js/03-fetch/pages/movies2.js
+++ react-next-js/03-fetch/pages/movies2.js
@@ -1,32 +1,65 @@
-import useSWR from "swr";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
-export default function Movies2() {
-  const { data, error } = useSWR(
-    `http://www.omdbapi.com/?apikey=67b600f&s=bagdad`,
-    fetcher
-  );
+export default function Movies() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchYear, setSearchYear] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  if (error) return <div>Falha na requisição...</div>;
-  if (!data) return <div>Carregando...</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const apiUrl = `http://www.omdbapi.com/?apikey=67b600f`;
+
+      let queryString = "";
+      if (searchTerm) {
+        queryString += `&s=${searchTerm}`;
+      }
+      if (searchYear) {
+        queryString += `&y=${searchYear}`;
+      }
+
+      const res = await fetch(`${apiUrl}${queryString}`);
+      const newData = await res.json();
+
+      setData(newData.Search || []);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [searchTerm, searchYear]);
 
   return (
     <div>
-      {data.Search.map((m) => (
-        <div key={m.imdbID}>
-          <Link href={`/movies/${m.imdbID}`}>
-            <img src={m.Poster} alt={m.Title} />
-            <p>{m.Title}</p>
-          </Link>
-        </div>
-      ))}
+      <form>
+        <input
+          type="text"
+          placeholder="Movie Name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Year"
+          value={searchYear}
+          onChange={(e) => setSearchYear(e.target.value)}
+        />
+      </form>
+      {loading && <p>Loading...</p>}
+      <div>
+        {data.map((element) => (
+          <div key={element.imdbID}>
+            <Link href={`/movies/${element.imdbID}`}>
+              <img src={element.Poster} alt={element.Title} />
+              <p>
+                {element.Title} --- {element.Year}
+              </p>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
-async function fetcher(url) {
-  const res = await fetch(url);
-  const json = await res.json();
-
-  return json;
 }
